35383ebbcb2eeaf54479f9ca0b41f1c0c7ad6c4e
medhaholla <holla.medha@gmail.com>
2022-12-16 11:13:39 +0530

    feat:establish db connection

---
+++ src/main/java/com/example/demotrueClub/DBoperations.java
@@ -0,0 +1,21 @@
+package com.example.demotrueClub;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBoperations {
+    public static volatile Connection connection;
+
+    public static Connection getConnection() throws SQLException {
+        if(connection == null){
+            synchronized (DBoperations.class){
+                if(connection == null){
+                    connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/TrueClub","root","@Adha2621");
+
+                }
+            }
+        }
+        return connection;
+    }
+}
